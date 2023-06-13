5f254dfe2258946bc0fe03616b117f1426d8e791
camilo004 <83430070+camilo004@users.noreply.github.com>
2023-06-13 18:39:33 -0500

    creacion front

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +12,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fcalendario';
+  fechasFestivas: string[] | undefined;
+  anoIngresado: number | undefined;
+  fechaSeleccionada!: Date;
+  respuesta: string | undefined;
+
+  constructor(private http: HttpClient, private snackBar: MatSnackBar) {}
+
+  
+
+  validarFechaFestiva() {
+    const dia = this.fechaSeleccionada.getDate();
+    const mes = this.fechaSeleccionada.getMonth() + 1;
+    const año = this.fechaSeleccionada.getFullYear();
+
+    this.http.get('http://localhost:8080/festivos/validar/' + dia + '/' + mes + '/' + año, { responseType: 'text' })
+      .subscribe(respuesta => {
+        this.mostrarAlerta(respuesta);
+      });
+  }
+
+  obtenerFechasFestivas() {
+    this.http.get<string[]>('http://localhost:8080/festivos/todos/' + this.anoIngresado)
+      .subscribe(fechasFestivas => {
+        this.fechasFestivas = fechasFestivas;
+        const mensaje = 'Fechas obtenidas correctamente:\n';
+        const fechas = this.fechasFestivas.map(fecha => '- ' + fecha).join('\n');
+        this.mostrarAlerta(mensaje + fechas);
+      });
+  }
+  mostrarAlerta(mensaje: string) {
+    this.snackBar.open(mensaje, 'Cerrar', {
+      horizontalPosition: 'center',
+      verticalPosition: 'top'
+    });
+  }
+ 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card'; 
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+
+
 
 @NgModule({
   declarations: [
@@ -10,7 +23,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatNativeDateModule,
+    MatInputModule,
+    FormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    BrowserAnimationsModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
